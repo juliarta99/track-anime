56a1ad2a4bf876ba46d41957b689d6d175574fe5
juliarta99 <adij4255@gmail.com>
2023-08-05 22:42:19 +0800

    add component navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+    return (
+        <>
+            <div className="w-100 py-2 px-5 bg-dark">
+                <div className="d-flex justify-content-between align-items-center">
+                    <h2 className="text-white">Track Anime</h2>
+                    <div className="d-flex gap-3">
+                        <a className="text-white">Test</a>
+                        <a className="text-white">Test</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
